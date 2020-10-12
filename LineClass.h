05733f0b8877f1b797c4234b95ad57999ceec101
VladLidyaev <vlad.lidyaev.01@mail.ru>
2020-10-12 16:55:53 +0300

    Update LineClass.h

--- LineClass.h
+++ LineClass.h
@@ -1,5 +1,7 @@
 #include <cmath>
 #include <iostream>
+#include <string.h>
+#include <sstream>
 
     struct Coord{
         float X;
@@ -11,8 +13,13 @@
             float param_a,param_b,param_c;
 
         public:
-            void SetDataParam(float new_param_a,float new_param_b,float new_param_c);
-            void GetData();
+            void SetDataParam_a(float a);
+            void SetDataParam_b(float b);
+            void SetDataParam_c(float c);
+            std::string GetData();
+            std::string GetData_a();
+            std::string GetData_b();
+            std::string GetData_c();
             float GetOrd(float Abs);
             float GetLen(float Abs1,float Abs2);
             float GetRadius(float Abs);
