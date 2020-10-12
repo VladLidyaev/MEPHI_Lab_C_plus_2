c2bf1b077e1340626bb36e49adf34df2a23451de
VladLidyaev <vlad.lidyaev.01@mail.ru>
2020-10-12 16:56:45 +0300

    Update Tools.h

--- Tools.h
+++ Tools.h
@@ -1,7 +1,9 @@
 #include "LineClass.h"
 #include <iostream>
 
-    Object ans_1(Object Line);
+    Object ans_1_a(Object Line);
+    Object ans_1_b(Object Line);
+    Object ans_1_c(Object Line);
     void ans_3(Object Line);
     void ans_4(Object Line);
     void ans_5(Object Line);
